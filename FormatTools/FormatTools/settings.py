c8725535d5d74db31db80ed4736f1e6d4e37c990
vaquer <akura11.tt@gmail.com>
2017-10-26 16:22:54 -0500

    Fix: Typo en settings

--- FormatTools/FormatTools/settings.py
+++ FormatTools/FormatTools/settings.py
@@ -68,7 +68,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'converter.f.global_conf'
+                'converter.context_processors.global_conf'
             ],
         },
     },
